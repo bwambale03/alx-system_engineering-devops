7556a1ea95d15e4ab562a27d5a2b14b3aae00033
bwambale03 <bwambaleedwin03@gmail.com>
2023-06-28 22:50:43 -0700

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,7 +5,6 @@ file_line { 'Turn off passwd auth':
   ensure = present,
   path  => '/etc/ssh/sshd_config',
   line  => 'PasswordAuthentication no',
-  match => '^#?PasswordAuthentication\s+',
   replace => true,
 }
 
@@ -13,6 +12,5 @@ file_line { 'Declare identity file':
    ensure = present,
   path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
-  match => '^#?IdentityFile\s+',
   replace => true,
 }
