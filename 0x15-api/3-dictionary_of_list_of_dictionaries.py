418bb2465e099c63ca330d32f1f2dbfcbd653290
bwambale03 <bwambaleedwin03@gmail.com>
2023-07-24 17:36:26 -0700

    replicating task #0, this script extends my Python script to export data in the JSON format.

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-""" Exports to-do list information of all employees to JSON format."""
+"""Exports to-do list information of all employees to JSON format."""
 import json
 import requests
 
+def get_employee_todo_list(employee_id):
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    response = requests.get(url)
+    return response.json()
+
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
+    base_url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(base_url + "users").json()
+
+    todo_data = {}
+    for user in users:
+        employee_id = user["id"]
+        employee_name = user["username"]
+        todo_data[employee_id] = [
+            {
+                "task": task["title"],
+                "completed": task["completed"],
+                "username": employee_name
+            }
+            for task in get_employee_todo_list(employee_id)
+        ]
 
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+        json.dump(todo_data, jsonfile, indent=2)
