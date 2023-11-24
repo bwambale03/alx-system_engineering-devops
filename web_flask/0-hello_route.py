47d32fe19c2d1237cc935a19f68306ad9c198655
Edwin Bwambale <bwambaleedwin03@gmail.com>
2023-11-24 06:28:04 +0000

    edited the nginx config file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,9 +7,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/airbnb-onepage/', strict_slashes=False)
-def index():
+def airbnb_onepage():
     """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+    return 'Hello from Airbnb Onepage!'
+# Add a route for the version without a trailing slash to redirect to the one with a trailing slash
+@app.route('/airbnb-onepage')
+def redirect_airbnb_onepage():
+    return redirect(url_for('airbnb_onepage'), code=301)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
